94620e85e811e94232f110c32351e97315375a62
rublen <rublen77@gmail.com>
2017-08-23 17:32:38 +0300

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,7 @@
 def bubble_sort_by(arr)
   (0...arr.size-1).each do |i|
     (0...arr.size-i-1).each do |j|
-      n = yield(arr[j], arr[j+1])
-      arr[j], arr[j+1] = arr[j+1], arr[j] if n > 0
+      arr[j], arr[j+1] = arr[j+1], arr[j] if yield(arr[j], arr[j+1]) > 0
     end
   end
   arr
